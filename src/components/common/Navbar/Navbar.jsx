ddd306305df6347fad3ab220d04bd1d899025480
sandipan chatterjee <sandipan.chatterjee8@gmail.com>
2025-01-30 13:21:57 +0530

    first draft

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 // components/Navbar.jsx
 import React from "react";
 import { Link } from "react-router-dom"; // Assuming you're using React Router
-import "./Navbar.css"; // Create this CSS file
+// import "./Navbar.css"; // Create this CSS file
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
+    <nav className="left-0 top-0 w-64 h-full bg-gray-800 text-white p-4">
+      <ul className="space-y-4">
+        <li>
+          <Link to="/" className="text-lg hover:text-gray-400">
             Home
           </Link>
         </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
+        <li>
+          <Link to="/about" className="text-lg hover:text-gray-400">
             About
           </Link>
         </li>
-        <li className="nav-item">
-          <Link to="/services" className="nav-link">
+        <li>
+          <Link to="/services" className="text-lg hover:text-gray-400">
             Services
           </Link>
         </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
+        <li>
+          <Link to="/contact" className="text-lg hover:text-gray-400">
             Contact
           </Link>
         </li>
